04333ba2412bf623cb7e46c6b6f4c7aaf3cfffec
zxeleven <john7233@gmail.com>
2019-05-03 16:45:03 -0500

    Fixed source

--- Emergency Vehicle Dispatch System/source.cpp
+++ Emergency Vehicle Dispatch System/source.cpp
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-  cout << "William is a clown ass boy!" << endl;
-  cout << "I take that back, he brought some snacks thru.";
-
+  cout << endl;
   return 0;
 }
-=======
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main(){
-  
-  return 0;
-}
->>>>>>> c6a0037da0f7b0616788efa3562b32a582c8e5ca
