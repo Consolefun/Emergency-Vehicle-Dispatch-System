8724f0eb38dee9b89dc5966cf548c54f68c8fd60
zxeleven <john7233@gmail.com>
2019-05-03 16:38:02 -0500

    EmergencyVehicle struct created

---
+++ Emergency Vehicle Dispatch System/source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "William is a clown ass boy!" << endl;
+  cout << "I take that back, he brought some snacks thru.";
+
+  return 0;
+}
