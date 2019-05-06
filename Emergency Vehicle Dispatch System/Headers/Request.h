23157733d86c9447c521cbb39eca8fb2fff453d3
zxeleven <john7233@gmail.com>
2019-05-06 17:14:27 -0500

    Updated request struct

--- Emergency Vehicle Dispatch System/Headers/Request.h
+++ Emergency Vehicle Dispatch System/Headers/Request.h
@@ -7,6 +7,21 @@ struct Request
   int type;
   int zipcode;
   int vehicle_id;
-}
+  int distance;
+
+  Request(int new_id, int new_type, int new_zip, int new_vehicle_id)
+  {
+	  id = new_id;
+	  type = new_type;
+	  zipcode = new_zip;
+	  vehicle_id = new_vehicle_id;
+  }
+
+  void fillRequest(int id, int dist)
+  {
+	  vehicle_id = id;
+	  distance = dist;
+  }
+};
 
 #endif /* end of include guard: */
