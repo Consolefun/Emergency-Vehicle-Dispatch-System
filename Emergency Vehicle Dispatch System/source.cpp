7814c729b0530e4eeb8531afd19a9b139c632796
zxeleven <john7233@gmail.com>
2019-05-06 17:15:08 -0500

    Read in requests

--- Emergency Vehicle Dispatch System/source.cpp
+++ Emergency Vehicle Dispatch System/source.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "./Headers/Graph.h"
+#include "./Headers/Request.h"
 
 using namespace std;
+
+void readInRequests(vector<Request>& requests, ifstream& in);
 
 int main()
 {
@@ -13,14 +16,16 @@ int main()
 	// validate that the input file was opened correctly
 	if (!vehicle_data || !distance_data || !request_data)
 	{
-		cerr << "There was an error while attemping to open one of the text files." << endl;
+		cerr << "There was an error attemping to open one of the text files." << endl;
 		system("pause");
 		exit(1);
 	}
 
 	Graph graph;
+	vector<Request> requests;
 	graph.populateGraph(vehicle_data);
 	graph.addEdges(distance_data);
+	readInRequests(requests, request_data);
 
 	/*for (int i = 0; i < graph.getGraph().size(); i++)
 	{
@@ -30,15 +35,18 @@ int main()
 		}
 	}*/
 
-	for (int i = 0; i < graph.getGraph().size(); i++)
+	/*for (int i = 0; i < graph.getGraph().size(); i++)
 	{
 		for (int j = 0; j < graph.getGraph()[i].getAdjacents().size(); j++)
 		{
 			cout << graph.getGraph()[i].getAdjacents()[j].target << " " << graph.getGraph()[i].getAdjacents()[j].distance <<  endl;
 		}
-	}
+	}*/
 
-	
+	/*for (int i = 0; i < requests.size(); i++)
+	{
+		cout << requests[i].id << " " << requests[i].type << " " << requests[i].zipcode << " " << requests[i].vehicle_id << endl;
+	}*/
 
 	// close all I/O streams
 	vehicle_data.close();
@@ -48,3 +56,19 @@ int main()
 	system("pause");
 	return 0;
 }
+
+void readInRequests(vector<Request>& requests, ifstream& in)
+{
+	int request_id = 0;
+	int vehicle_type = 0;
+	int zip = 0;
+	int vehicle_id = 0;
+
+	while (in >> request_id >> vehicle_type >> zip >> vehicle_id)
+	{
+		Request new_request(request_id, vehicle_type, zip, vehicle_id);
+		requests.push_back(new_request);
+	}
+}
+
+
