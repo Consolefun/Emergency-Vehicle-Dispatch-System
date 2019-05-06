3cc032ff89ae27a56427b28a0468cb85ca397125
zxeleven <john7233@gmail.com>
2019-05-05 19:20:44 -0500

    Added constructors

---
+++ Emergency Vehicle Dispatch System/Headers/EmergencyVehicle.h
@@ -0,0 +1,26 @@
+#ifndef EMERGENCYVEHICLE_H
+#define EMERGENCYVEHICLE_H
+
+#include <stdbool.h>
+
+struct EmergencyVehicle
+{
+  int id;
+  int type;
+  bool available;
+
+  EmergencyVehicle()
+  {
+	  id, type = 0;
+	  available = true;
+  }
+
+  EmergencyVehicle(int new_id, int t)
+  {
+	  id = new_id;
+	  type = t;
+	  available = true;
+  }
+};
+
+#endif /* end of include guard: */
